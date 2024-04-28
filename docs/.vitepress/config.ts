3e9eb924298ba6504112bbed68d02dd56b5ee499
raotao <>
2024-04-28 20:07:56 +0800

    deploy: change publish base path

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from 'vitepress';
+
+//github仓库名称
+const repo = 'ez-lib-template';
+
+const isProd = process.env.NODE_ENV === 'production';
+const base = isProd ? `/${repo}/` : '/';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "My Awesome Project",
-  description: "A VitePress Site",
+  base,
+  title: 'My Awesome Project',
+  description: 'A VitePress Site',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Examples', link: '/markdown-examples' },
     ],
 
     sidebar: [
@@ -16,13 +23,11 @@ export default defineConfig({
         text: 'Examples',
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
     ],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+  },
+});
